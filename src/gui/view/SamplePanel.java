1cb0627687714a7bd83892c29314cd60dd3f0d5b
Shahriar <ssha7332@gmail.com>
2018-11-27 13:11:10 -0700

    made a finished project.

--- src/gui/view/SamplePanel.java
+++ src/gui/view/SamplePanel.java
@@ -2,16 +2,63 @@ package gui.view;
 
 import javax.swing.*;
 import gui.controller.GUIController;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Color;
 
 public class SamplePanel extends JPanel
 {
 	private GUIController appController;
+	private JLabel textLabel;
+	private JButton colorButton;
 	
 	public SamplePanel(GUIController appController)
 	{
 		super();
 		
 		this.appController = appController;
+		
+		this.textLabel = new JLabel("This is a color app!");
+		this.colorButton = new JButton("Click here to change color");
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
 	}
-
+	
+	private void setupPanel()
+	{
+		this.setBackground(Color.DARK_GRAY);
+		this.add(textLabel);
+		this.add(colorButton);
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+	{
+		colorButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent mouseClick)
+			{
+				changeBackgroundColor();
+			}
+		});
+	}
+	
+	private void changeBackgroundColor()
+	{
+		int red = (int) (Math.random() * 256);
+		int green = (int) (Math.random() * 256);
+		int blue = (int) (Math.random() * 256);
+		
+		this.setBackground(new Color(red, green, blue));
+		textLabel.setText("Red: " + red + " Green: " + green + " Blue: " + blue);
+	}
 }
